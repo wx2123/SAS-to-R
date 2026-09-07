81dc61327f3ce9a7fc3c7b8a7870ca8cfa57e57d
wx2123 <xuewujian@hotmail.com>
2026-09-06 22:04:33 -0400

    Update code.R

--- code.R
+++ code.R
@@ -5,3 +5,9 @@ library(haven)
 
 df <- read_sas("C:/Users/xuewu/Downloads/dates.sas7bdat")
 saveRDS(df, file = "C:/Users/xuewu/Downloads/df.rds")
+
+
+df <-     read_sas("C:/2026/2601 ASA/2608 SSDS/SAS-to-R/loan_data.sas7bdat")
+saveRDS(df, file = "C:/2026/2601 ASA/2608 SSDS/SAS-to-R/loan_data.rds")
+
+
